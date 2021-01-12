66fbe1dd8210bb4e9e4942334c768d842344601c
Vlada M <vladyslavaa.m@gmail.com>
2021-01-12 21:57:48 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace KGV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orchid;
+            this.button1.Location = new System.Drawing.Point(41, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ковер Серпинского";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orchid;
+            this.button2.Location = new System.Drawing.Point(41, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Бассейн Ньютона";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(41, 132);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(143, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged_1);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Thistle;
+            this.button4.Location = new System.Drawing.Point(60, 363);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 51);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.GhostWhite;
+            this.pictureBox1.Location = new System.Drawing.Point(252, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(529, 509);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
+            this.ClientSize = new System.Drawing.Size(793, 533);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KGV
+{
+    public partial class Form1 : Form
+    {
+        struct Complex
+        {
+            public double x;
+            public double y;
+
+        };
+
+        const int iter = 50;
+        const double min = 1e-6;
+        const double max = 1e+6;
+
+        private int Level = 0;
+        //Высота и ширина для отрисовки
+        private int width;
+        private int height;
+        // Bitmap для фрактала
+        private Bitmap fractal;
+        // используем для отрисовки на PictureBox
+        private Graphics graph;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //инициализируем ширину и высоту
+            width = pictureBox1.Width;
+            height = pictureBox1.Height;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //создаем Bitmap для прямоугольника
+            fractal = new Bitmap(width, height);
+            // cоздаем новый объект Graphics из указанного Bitmap
+            graph = Graphics.FromImage(fractal);
+            //создаем прямоугольник и вызываем функцию отрисовки ковра
+            RectangleF carpet = new RectangleF(0, 0, width, height);
+            DrawCarpet(Level, carpet);
+            //отображаем результат
+            pictureBox1.BackgroundImage = fractal;
+        }
+        private void numericUpDown1_ValueChanged_1(object sender, EventArgs e)
+        {
+            Level = Convert.ToInt32(numericUpDown1.Value);
+
+            fractal = new Bitmap(width, height);
+            graph = Graphics.FromImage(fractal);
+            RectangleF carpet = new RectangleF(0, 0, width, height);
+            DrawCarpet(Level, carpet);
+            pictureBox1.BackgroundImage = fractal;
+        }
+
+        private void DrawCarpet(int level, RectangleF carpet)
+        {
+            //проверяем, закончили ли мы построение
+            if (level == 0)
+            {
+                //Рисуем прямоугольник
+                graph.FillRectangle(Brushes.MediumPurple, carpet);
+            }
+            else
+            {
+                // делим прямоугольник на 9 частей
+                var width = carpet.Width / 3f;
+                var height = carpet.Height / 3f;
+                // (x1, y1) - координаты левой верхней вершины прямоугольника
+                // от нее будем отсчитывать остальные вершины маленьких прямоугольников
+                var x1 = carpet.Left;
+                var x2 = x1 + width;
+                var x3 = x1 + 2f * width;
+
+                var y1 = carpet.Top;
+                var y2 = y1 + height;
+                var y3 = y1 + 2f * height;
+
+                DrawCarpet(level - 1, new RectangleF(x1, y1, width, height)); // левый 1(верхний)
+                DrawCarpet(level - 1, new RectangleF(x2, y1, width, height)); // средний 1
+                DrawCarpet(level - 1, new RectangleF(x3, y1, width, height)); // правый 1
+                DrawCarpet(level - 1, new RectangleF(x1, y2, width, height)); // левый 2
+                DrawCarpet(level - 1, new RectangleF(x3, y2, width, height)); // правый 2
+                DrawCarpet(level - 1, new RectangleF(x1, y3, width, height)); // левый 3
+                DrawCarpet(level - 1, new RectangleF(x2, y3, width, height)); // средний 3
+                DrawCarpet(level - 1, new RectangleF(x3, y3, width, height)); // правый 3
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Создаем перо цвета - Черный(Black)
+            //Толщина - 1 пиксель:
+            Pen myPen = new Pen(Color.Black, 1);
+            //Объявляем объект "g" класса Graphics и предоставляем
+            //ему возможность рисования на pictureBox1:
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            //Вызов функции отрисовки фрактала
+            //int mx =1026, my = 428;
+            int mx = pictureBox1.Width, my = pictureBox1.Height;
+
+            DrawPool(mx, my, g, myPen);
+        }
+
+        public void DrawPool(int mx1, int my1, Graphics g, Pen pen)
+        {
+            int n, mx, my;
+            double p;
+            Complex z, t, d = new Complex();
+
+            mx = mx1 / 2;
+            my = my1 / 2;
+
+            for (int y = -my; y < my; y++)
+                for (int x = -mx; x < mx; x++)
+                {
+                    n = 0;
+                    z.x = x * 0.005;
+                    z.y = y * 0.005;
+                    d = z;
+
+                    while ((Math.Pow(z.x, 2) + Math.Pow(z.y, 2) < max) && (Math.Pow(d.x, 2) + Math.Pow(d.y, 2) > min) && (n < iter))
+                    {
+                        t = z;
+                        p = Math.Pow(Math.Pow(t.x, 2) + Math.Pow(t.y, 2), 2);
+                        z.x = 2 / 3 * t.x + (Math.Pow(t.x, 2) - Math.Pow(t.y, 2)) / (3 * p);
+                        z.y = 2 / 3 * t.y * (1 - t.x / p);
+                        d.x = Math.Abs(t.x - z.x);
+                        d.y = Math.Abs(t.y - z.y);
+                        n++;
+                    }
+                    pen.Color = Color.FromArgb(255, (n * 9) % 255, 0, (n * 9) % 255);
+                    g.DrawRectangle(pen, mx + x, my + y, 1, 1);
+
+                }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 0;
+            pictureBox1.BackgroundImage = null;
+            pictureBox1.Image = null;
+            pictureBox1.Invalidate();
+        }
+    }
+}
